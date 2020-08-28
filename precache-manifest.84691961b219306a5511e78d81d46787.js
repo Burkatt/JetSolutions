63bbc470e538d69bf3d8985d6ea8b93027c2cd3f
Таня Бура <tanya.bura.99@gmail.com>
2020-08-28 21:56:14 +0300

    Updates

---
+++ precache-manifest.84691961b219306a5511e78d81d46787.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "916082ac608febbfd66b5a1209680195",
+    "url": "/jet_solutins/index.html"
+  },
+  {
+    "revision": "11cc2fdad4b73a8d1154",
+    "url": "/jet_solutins/static/css/main.3331e8c5.chunk.css"
+  },
+  {
+    "revision": "c092f2717fd3c60da2f9",
+    "url": "/jet_solutins/static/js/2.38a742e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jet_solutins/static/js/2.38a742e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11cc2fdad4b73a8d1154",
+    "url": "/jet_solutins/static/js/main.427319c0.chunk.js"
+  },
+  {
+    "revision": "7d9a2cd7f0fee091fe18",
+    "url": "/jet_solutins/static/js/runtime-main.b01d2957.js"
+  },
+  {
+    "revision": "fa3aaf3f7399cdc5be899828ea6e7582",
+    "url": "/jet_solutins/static/media/animal.fa3aaf3f.jpg"
+  },
+  {
+    "revision": "d6ac6edc70de7c814f48872e57cf0c95",
+    "url": "/jet_solutins/static/media/elephant.d6ac6edc.jpg"
+  },
+  {
+    "revision": "9aec3088e2e2e22ea5c1e41e52077afd",
+    "url": "/jet_solutins/static/media/fox.9aec3088.jpg"
+  },
+  {
+    "revision": "5355e6aa0ab2665bb884ada056ff7a5f",
+    "url": "/jet_solutins/static/media/horse.5355e6aa.jpg"
+  },
+  {
+    "revision": "dab9217067ea094eec0ed909d1114019",
+    "url": "/jet_solutins/static/media/lazy.dab92170.jpg"
+  },
+  {
+    "revision": "ca2b91ddd6a576745b49c9a8501fe437",
+    "url": "/jet_solutins/static/media/lion.ca2b91dd.jpg"
+  },
+  {
+    "revision": "9f1b96bdf0c7fb98b09d1180488fef45",
+    "url": "/jet_solutins/static/media/logo.9f1b96bd.jpg"
+  },
+  {
+    "revision": "3d4e6ed00a7473b81304f62ae84f457f",
+    "url": "/jet_solutins/static/media/panda.3d4e6ed0.jpg"
+  },
+  {
+    "revision": "98b09053e24ce48efc945a3830a3d2e2",
+    "url": "/jet_solutins/static/media/pingvi.98b09053.jpg"
+  }
+]);
